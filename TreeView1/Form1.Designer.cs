2098f762bf1c32db8718a06f4449674840861f87
Hae <foreletters@hotmail.com>
2018-03-30 01:24:09 -0400

    Commit

---
+++ TreeView1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TreeView1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvListOfAssets = new System.Windows.Forms.TreeView();
+            this.btnTopDownExecute = new System.Windows.Forms.Button();
+            this.btnBottomUpExecute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tvListOfAssets
+            // 
+            this.tvListOfAssets.Location = new System.Drawing.Point(180, 85);
+            this.tvListOfAssets.Name = "tvListOfAssets";
+            this.tvListOfAssets.Size = new System.Drawing.Size(877, 1318);
+            this.tvListOfAssets.TabIndex = 0;
+            // 
+            // btnTopDownExecute
+            // 
+            this.btnTopDownExecute.Location = new System.Drawing.Point(1518, 972);
+            this.btnTopDownExecute.Name = "btnTopDownExecute";
+            this.btnTopDownExecute.Size = new System.Drawing.Size(468, 148);
+            this.btnTopDownExecute.TabIndex = 1;
+            this.btnTopDownExecute.Text = "button1";
+            this.btnTopDownExecute.UseVisualStyleBackColor = true;
+            this.btnTopDownExecute.Click += new System.EventHandler(this.btnTopDownExecute_Click);
+            // 
+            // btnBottomUpExecute
+            // 
+            this.btnBottomUpExecute.Location = new System.Drawing.Point(1518, 1218);
+            this.btnBottomUpExecute.Name = "btnBottomUpExecute";
+            this.btnBottomUpExecute.Size = new System.Drawing.Size(449, 130);
+            this.btnBottomUpExecute.TabIndex = 2;
+            this.btnBottomUpExecute.Text = "button2";
+            this.btnBottomUpExecute.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2293, 1592);
+            this.Controls.Add(this.btnBottomUpExecute);
+            this.Controls.Add(this.btnTopDownExecute);
+            this.Controls.Add(this.tvListOfAssets);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvListOfAssets;
+        private System.Windows.Forms.Button btnTopDownExecute;
+        private System.Windows.Forms.Button btnBottomUpExecute;
+    }
+}
+
---
+++ TreeView1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeView1
+{
+    public partial class Form1 : Form
+    {
+        TreeNode tn1 = new TreeNode();
+        TreeNode tn2 = new TreeNode();
+        TreeNode tn3 = new TreeNode();
+        Queue<KeyValuePair<string, string>> q1 = new Queue<KeyValuePair<string, string>>();
+        Queue<KeyValuePair<string, string>> q2 = new Queue<KeyValuePair<string, string>>();
+        Queue<KeyValuePair<string, string>> q3 = new Queue<KeyValuePair<string, string>>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tn1.Text = "Cash";
+            tn2.Text = "Fund";
+            tn3.Text = "Non-Cash Non-Fund";
+            tvListOfAssets.Nodes.Add(tn1);
+            tvListOfAssets.Nodes.Add(tn2);
+            tvListOfAssets.Nodes.Add(tn3);
+            q1.Enqueue(new KeyValuePair<string, string>("Cash", "USD"));
+            q1.Enqueue(new KeyValuePair<string, string>("Cash", "GBP"));
+            q1.Enqueue(new KeyValuePair<string, string>("Cash", "EUR"));
+            q1.Enqueue(new KeyValuePair<string, string>("Non-Cash Non-Fund", "CitiGroup"));
+            q1.Enqueue(new KeyValuePair<string, string>("Fund", "GLD"));
+            q1.Enqueue(new KeyValuePair<string, string>("Fund", "VIX"));
+            q1.Enqueue(new KeyValuePair<string, string>("Fund", "Something Else"));
+            q1.Enqueue(new KeyValuePair<string, string>("Non-Cash Non-Fund", "GS"));
+            q1.Enqueue(new KeyValuePair<string, string>("Non-Cash Non-Fund", "AMG"));
+
+            q2.Enqueue(new KeyValuePair<string, string>("AMG", "Stock"));
+            q2.Enqueue(new KeyValuePair<string, string>("CitiGroup", "Stock"));
+            q2.Enqueue(new KeyValuePair<string, string>("VIX", "ADR"));
+            q2.Enqueue(new KeyValuePair<string, string>("GBP", "ADR"));
+
+            q3.Enqueue(new KeyValuePair<string, string>("Stock", "Domestic Asset"));
+            q3.Enqueue(new KeyValuePair<string, string>("Stock", "Domestic Asset"));
+            q3.Enqueue(new KeyValuePair<string, string>("ADR", "Foreign Asset"));
+            q3.Enqueue(new KeyValuePair<string, string>("ADR", "Foreign Asset"));
+
+            while (q1.Count > 0)
+            {
+                foreach (TreeNode node in tvListOfAssets.Nodes)
+                {
+                    AddAssets(node, q1);
+                    
+                }
+            }
+            while(q2.Count>0)
+            {
+                foreach(TreeNode node in tvListOfAssets.Nodes)
+                {
+                    foreach(TreeNode tn in node.Nodes)
+                    {
+                        AddAssets(tn, q2);
+                    }
+                }
+            }
+            while(q3.Count>0)
+            {
+                foreach(TreeNode node in tvListOfAssets.Nodes)
+                {
+                    foreach(TreeNode tn1 in node.Nodes)
+                    {
+                        foreach(TreeNode tn2 in tn1.Nodes)
+                        {
+                            AddAssets(tn2, q3);
+                        }
+                    }
+                }
+            }
+        }
+        private void AddAssets(TreeNode  node, Queue<KeyValuePair<string,string>> q)
+        {
+            for (int i = 0; i < q.Count; i++)
+            {
+                KeyValuePair<string, string> kvp = q.Dequeue();
+                if (node.Text == kvp.Key)
+                {
+                    node.Nodes.Add(kvp.Value);
+                }
+                else
+                {
+                    q.Enqueue(kvp);
+                }
+            }
+            
+            
+        }
+
+        private void btnTopDownExecute_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void TopDown(TreeNode node)
+        {
+
+        }
+        private void BottomUp(TreeNode node)
+        {
+
+        }
+    }
+}
